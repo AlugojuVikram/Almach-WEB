fbe9fbbb926717150f3523330da87c8f81816fbe
Alugoju vikram <vicky.vikram.71099@gmail.com>
2022-03-14 16:39:06 +0530

    updated

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\fluttersdk\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Almach-Web" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\fluttersdk\\flutter"
+  "PROJECT_DIR=D:\\Almach-Web"
+  "FLUTTER_ROOT=E:\\fluttersdk\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Almach-Web\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Almach-Web"
+  "FLUTTER_TARGET=D:\\Almach-Web\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Almach-Web\\.dart_tool\\package_config.json"
+)
